dbd0b038e9cc90f8a31d225ad42492444b542a3c
joshua1988 <jangkeehyo@gmail.com>
2018-02-20 17:51:54 +0900

    Added the sample codes used in the presentation

---
+++ template-compile/app.vue
@@ -0,0 +1,25 @@
+<!-- #1 -->
+<template>
+  <div>
+  	<section>
+  		Do it! Vue.js 입문
+  	</section>
+		<p>
+			출간 기념 공개 세미나
+		</p>
+  </div>
+</template>
+
+<!-- #2 -->
+<script>
+	render: function (createElement) {
+		return createElement('div', [
+			createElement(
+				'section',
+				'Do it! Vue.js 입문'),
+			createElement(
+				'p',
+				'출간 기념 공개 세미나')
+		]);
+	}
+</script>
